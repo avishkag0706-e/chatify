d2c102c7c0d5b0c6a24208871204d81bb983efe9
avishka <avishka.g0706@gmail.com>
2026-08-30 11:49:55 +0530

    Prepare backend for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path"
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve(); 
 
-const PORT= process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//make ready for deployment
-if(process.env.NODE_ENV ==="production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Live Chat Backend is running"
+    });
+});
 
-
-app.listen(PORT,()=>{console.log("server running on port 3000")});
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
